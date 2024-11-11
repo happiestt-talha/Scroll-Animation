39b38b975136886eaf5e7212255f28398388723e
Talha <mtalhamanzoor786@gmail.com>
2024-11-11 23:26:27 +0500

    styled sections

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Section = ({ children }) => {
     return (
         <>
-            <div className="h-screen w-full">
+            <div className="h-screen w-full flex items-center justify-center">
                 {children}
             </div>
         </>
